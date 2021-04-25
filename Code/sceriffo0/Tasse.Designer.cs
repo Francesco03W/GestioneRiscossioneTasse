447ed47e6533aa8aef26f96bae53efbbf8b15195
Francesco03W <francescoveronese16@gmail.com>
2021-04-25 17:06:26 +0200

    Beta 0.0.5
    Aggiunto Crittografia e serializzazione password nella registrazione, aggiuto decrittografia e deserializzazione per il login, manca solo il controllo : se la password è la stessa di quella registrata.

--- Code/sceriffo0/Tasse.Designer.cs
+++ Code/sceriffo0/Tasse.Designer.cs
@@ -28,30 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tasse1));
             this.panelLogIn = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.srittaPassword = new System.Windows.Forms.Label();
             this.bottoneAccedi = new System.Windows.Forms.Button();
             this.ScrittaLogIn = new System.Windows.Forms.Label();
-            this.srittaPassword = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PannelloAltoSX = new System.Windows.Forms.Panel();
+            this.pannelRegistrazione = new System.Windows.Forms.Panel();
+            this.bottoneInvioRegistrazione = new System.Windows.Forms.Button();
+            this.contenitorePasswordInserita = new System.Windows.Forms.TextBox();
+            this.scrittaIstruzioniPassword = new System.Windows.Forms.Label();
+            this.scrittaRegistrazione = new System.Windows.Forms.Label();
+            this.PannelloAltoDX = new System.Windows.Forms.Panel();
+            this.PannelloBassoSX = new System.Windows.Forms.Panel();
+            this.PannelloBassoDX = new System.Windows.Forms.Panel();
             this.panelLogIn.SuspendLayout();
+            this.pannelRegistrazione.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLogIn
             // 
+            this.panelLogIn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelLogIn.Controls.Add(this.textBox1);
             this.panelLogIn.Controls.Add(this.srittaPassword);
             this.panelLogIn.Controls.Add(this.bottoneAccedi);
             this.panelLogIn.Controls.Add(this.ScrittaLogIn);
-            this.panelLogIn.Location = new System.Drawing.Point(496, 12);
+            this.panelLogIn.Location = new System.Drawing.Point(541, 219);
             this.panelLogIn.Name = "panelLogIn";
-            this.panelLogIn.Size = new System.Drawing.Size(330, 391);
+            this.panelLogIn.Size = new System.Drawing.Size(318, 208);
             this.panelLogIn.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // srittaPassword
+            // 
+            this.srittaPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.srittaPassword.Location = new System.Drawing.Point(80, 75);
+            this.srittaPassword.Name = "srittaPassword";
+            this.srittaPassword.Size = new System.Drawing.Size(159, 24);
+            this.srittaPassword.TabIndex = 2;
+            this.srittaPassword.Text = "Inserire Password";
+            this.srittaPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // bottoneAccedi
             // 
             this.bottoneAccedi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.bottoneAccedi.Location = new System.Drawing.Point(120, 156);
+            this.bottoneAccedi.Location = new System.Drawing.Point(124, 149);
             this.bottoneAccedi.Name = "bottoneAccedi";
             this.bottoneAccedi.Size = new System.Drawing.Size(76, 27);
             this.bottoneAccedi.TabIndex = 1;
@@ -63,28 +90,99 @@
             // 
             this.ScrittaLogIn.AutoSize = true;
             this.ScrittaLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.ScrittaLogIn.Location = new System.Drawing.Point(114, 10);
+            this.ScrittaLogIn.Location = new System.Drawing.Point(118, 31);
             this.ScrittaLogIn.Name = "ScrittaLogIn";
             this.ScrittaLogIn.Size = new System.Drawing.Size(82, 31);
             this.ScrittaLogIn.TabIndex = 0;
             this.ScrittaLogIn.Text = "LogIn";
             // 
-            // srittaPassword
+            // PannelloAltoSX
             // 
-            this.srittaPassword.AutoSize = true;
-            this.srittaPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.srittaPassword.Location = new System.Drawing.Point(81, 84);
-            this.srittaPassword.Name = "srittaPassword";
-            this.srittaPassword.Size = new System.Drawing.Size(159, 24);
-            this.srittaPassword.TabIndex = 2;
-            this.srittaPassword.Text = "Inserire Password";
+            this.PannelloAltoSX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PannelloAltoSX.Location = new System.Drawing.Point(10, 11);
+            this.PannelloAltoSX.Name = "PannelloAltoSX";
+            this.PannelloAltoSX.Size = new System.Drawing.Size(502, 338);
+            this.PannelloAltoSX.TabIndex = 1;
             // 
-            // textBox1
+            // pannelRegistrazione
             // 
-            this.textBox1.Location = new System.Drawing.Point(51, 111);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 20);
-            this.textBox1.TabIndex = 3;
+            this.pannelRegistrazione.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pannelRegistrazione.Controls.Add(this.bottoneInvioRegistrazione);
+            this.pannelRegistrazione.Controls.Add(this.contenitorePasswordInserita);
+            this.pannelRegistrazione.Controls.Add(this.scrittaIstruzioniPassword);
+            this.pannelRegistrazione.Controls.Add(this.scrittaRegistrazione);
+            this.pannelRegistrazione.Location = new System.Drawing.Point(541, 26);
+            this.pannelRegistrazione.Margin = new System.Windows.Forms.Padding(2);
+            this.pannelRegistrazione.Name = "pannelRegistrazione";
+            this.pannelRegistrazione.Size = new System.Drawing.Size(337, 188);
+            this.pannelRegistrazione.TabIndex = 4;
+            // 
+            // bottoneInvioRegistrazione
+            // 
+            this.bottoneInvioRegistrazione.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bottoneInvioRegistrazione.Location = new System.Drawing.Point(116, 151);
+            this.bottoneInvioRegistrazione.Margin = new System.Windows.Forms.Padding(2);
+            this.bottoneInvioRegistrazione.Name = "bottoneInvioRegistrazione";
+            this.bottoneInvioRegistrazione.Size = new System.Drawing.Size(94, 32);
+            this.bottoneInvioRegistrazione.TabIndex = 3;
+            this.bottoneInvioRegistrazione.Text = "Registrati";
+            this.bottoneInvioRegistrazione.UseVisualStyleBackColor = true;
+            this.bottoneInvioRegistrazione.Click += new System.EventHandler(this.BottoneInvioRegistrazione_Click);
+            // 
+            // contenitorePasswordInserita
+            // 
+            this.contenitorePasswordInserita.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.contenitorePasswordInserita.Location = new System.Drawing.Point(25, 109);
+            this.contenitorePasswordInserita.Margin = new System.Windows.Forms.Padding(2);
+            this.contenitorePasswordInserita.Name = "contenitorePasswordInserita";
+            this.contenitorePasswordInserita.Size = new System.Drawing.Size(284, 26);
+            this.contenitorePasswordInserita.TabIndex = 2;
+            // 
+            // scrittaIstruzioniPassword
+            // 
+            this.scrittaIstruzioniPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.scrittaIstruzioniPassword.Location = new System.Drawing.Point(15, 53);
+            this.scrittaIstruzioniPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scrittaIstruzioniPassword.Name = "scrittaIstruzioniPassword";
+            this.scrittaIstruzioniPassword.Size = new System.Drawing.Size(294, 45);
+            this.scrittaIstruzioniPassword.TabIndex = 1;
+            this.scrittaIstruzioniPassword.Text = "Inserire una password tra gli 8 e i 16 caratteri";
+            this.scrittaIstruzioniPassword.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // scrittaRegistrazione
+            // 
+            this.scrittaRegistrazione.AutoSize = true;
+            this.scrittaRegistrazione.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.scrittaRegistrazione.Location = new System.Drawing.Point(85, 17);
+            this.scrittaRegistrazione.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scrittaRegistrazione.Name = "scrittaRegistrazione";
+            this.scrittaRegistrazione.Size = new System.Drawing.Size(151, 26);
+            this.scrittaRegistrazione.TabIndex = 0;
+            this.scrittaRegistrazione.Text = "Registrazione ";
+            // 
+            // PannelloAltoDX
+            // 
+            this.PannelloAltoDX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PannelloAltoDX.Location = new System.Drawing.Point(906, 12);
+            this.PannelloAltoDX.Name = "PannelloAltoDX";
+            this.PannelloAltoDX.Size = new System.Drawing.Size(349, 330);
+            this.PannelloAltoDX.TabIndex = 2;
+            // 
+            // PannelloBassoSX
+            // 
+            this.PannelloBassoSX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PannelloBassoSX.Location = new System.Drawing.Point(10, 348);
+            this.PannelloBassoSX.Name = "PannelloBassoSX";
+            this.PannelloBassoSX.Size = new System.Drawing.Size(622, 323);
+            this.PannelloBassoSX.TabIndex = 3;
+            // 
+            // PannelloBassoDX
+            // 
+            this.PannelloBassoDX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PannelloBassoDX.Location = new System.Drawing.Point(637, 348);
+            this.PannelloBassoDX.Name = "PannelloBassoDX";
+            this.PannelloBassoDX.Size = new System.Drawing.Size(618, 323);
+            this.PannelloBassoDX.TabIndex = 4;
             // 
             // Tasse1
             // 
@@ -92,13 +190,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pannelRegistrazione);
             this.Controls.Add(this.panelLogIn);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Controls.Add(this.PannelloAltoSX);
+            this.Controls.Add(this.PannelloAltoDX);
+            this.Controls.Add(this.PannelloBassoSX);
+            this.Controls.Add(this.PannelloBassoDX);
             this.Name = "Tasse1";
-            this.Text = "Sceriffo ";
+            this.Text = "Gestione Riscossione Tasse";
             this.Load += new System.EventHandler(this.Tasse1_Load);
             this.panelLogIn.ResumeLayout(false);
             this.panelLogIn.PerformLayout();
+            this.pannelRegistrazione.ResumeLayout(false);
+            this.pannelRegistrazione.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -110,6 +214,15 @@
         private System.Windows.Forms.Button bottoneAccedi;
         private System.Windows.Forms.Label srittaPassword;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel PannelloAltoSX;
+        private System.Windows.Forms.Panel PannelloAltoDX;
+        private System.Windows.Forms.Panel PannelloBassoSX;
+        private System.Windows.Forms.Panel PannelloBassoDX;
+        private System.Windows.Forms.Panel pannelRegistrazione;
+        private System.Windows.Forms.Button bottoneInvioRegistrazione;
+        private System.Windows.Forms.TextBox contenitorePasswordInserita;
+        private System.Windows.Forms.Label scrittaIstruzioniPassword;
+        private System.Windows.Forms.Label scrittaRegistrazione;
     }
 }
 
--- Code/sceriffo0/Tasse.cs
+++ Code/sceriffo0/Tasse.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//INFORMAZIONI IMPORTANTI DA NON TOCCARE:
+/*
+ if you use the hash (outdated md5) of a secret string (too short to be secure) to encrypt the password there is no point in hashing the secret here I think
+ tripleDES is also deprecated
+ storing secrets in code is security issue, but I think it's a practice program so it's no big deal (?))
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,28 +14,243 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+
 //https://www.youtube.com/watch?v=5Anto1N_FSs
 namespace sceriffo0
 {
     public partial class Tasse1 : Form
     {
-        //List<Panel> listPanel = new List<Panel>();
+        // DA FINIRE *non toccare*:
+        // mettere l'inserimento della password con il pulsante "accedi" e controllare che sia uguale a quella già impostata nell'User.Password
+
+        Utente User = new Utente(); // - ok
+        Utente Userload = new Utente(); //test, non badateci
+
         public Tasse1()
         {
             InitializeComponent();
+           
         }
 
         private void Tasse1_Load(object sender, EventArgs e)
         {
 
-            //listPanel.Add(panelLogIn);
-            panelLogIn.Visible = true;
+            pannelRegistrazione.Visible = false;
+            panelLogIn.Visible = false;
+            PannelloAltoDX.Visible = false;
+            PannelloAltoSX.Visible = false;
+            PannelloBassoSX.Visible = false;
+            PannelloBassoDX.Visible = false;
+            if (Directory.Exists(@"Login") == true)
+            {
+                //deserializzazione del file 
+                panelLogIn.Visible = true;
+                User = JsonConvert.DeserializeObject<Utente>(File.ReadAllText(String.Format(@"Login\\datilogin.json")));
+                //la password viene decrittata secondo lo stesso hash con cui è stata crittata
+                string hashDecritt = "cr1tt0gr4f1@";
+
+                byte[] datiPasswordDec = Convert.FromBase64String(User.Password);
+                using(MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] chiavi = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hashDecritt));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = chiavi, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform convertitoreDecritto = tripDes.CreateDecryptor();
+                        byte[] convertitoDecritto = convertitoreDecritto.TransformFinalBlock(datiPasswordDec, 0, datiPasswordDec.Length);
+                        //prima si deserializza, poi si decodifica
+
+                        User.Password = UTF8Encoding.UTF8.GetString(convertitoDecritto);
+
+                    }
+
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(@"Login");
+                pannelRegistrazione.Visible = true;
+            }
 
         }
 
         private void bottoneAccedi_Click(object sender, EventArgs e)
         {
             panelLogIn.Visible = false;
+            
+
         }
+
+        private void BottoneInvioRegistrazione_Click(object sender, EventArgs e)
+        {
+            char lettpass;
+            int lettCAPS=0;
+            int lettNum=0;
+            if (string.IsNullOrWhiteSpace(contenitorePasswordInserita.Text) == false)
+            {
+                //facciamo che la password deve contenere almeno 1 numero e 1 lettera maiuscola
+                for (int i = 0; i < contenitorePasswordInserita.TextLength; i++)
+                {
+                    //si preleva ogni singolo carattere dalla password
+                    lettpass = Convert.ToChar(contenitorePasswordInserita.Text.Substring(i, 1));
+                    if (Char.IsUpper(lettpass) == true)
+                    {
+                        lettCAPS++;
+                    }
+                    if (Char.IsDigit(lettpass) == true)
+                    {
+                        lettNum++;
+                    }
+                }
+                if (lettCAPS >= 1 && lettNum >= 1)
+                {
+                    if (contenitorePasswordInserita.TextLength >= 8 & contenitorePasswordInserita.TextLength <= 16)
+                    {
+                        //allora la password va bene e viene salvata
+                        User.Password = contenitorePasswordInserita.Text;
+                        //la password va crittata, serializzata e messa in datilogin.json
+                        string hashCritt = "cr1tt0gr4f1@";
+                        byte[] datiPasswordEnc = UTF8Encoding.UTF8.GetBytes(User.Password);
+                        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                        {
+                            byte[] chiavi = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hashCritt));
+                            using (TripleDESCryptoServiceProvider tripDES = new TripleDESCryptoServiceProvider() { Key = chiavi, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                            {
+                                ICryptoTransform convertitoreCritto = tripDES.CreateEncryptor();
+                                byte[] ConvertitoCritto = convertitoreCritto.TransformFinalBlock(datiPasswordEnc, 0, datiPasswordEnc.Length);
+                                User.Password = Convert.ToBase64String(ConvertitoCritto, 0, ConvertitoCritto.Length);
+                            }
+                        }
+                        //la password crittata viene serializzata
+                        File.Create(@"Login\\datilogin.json").Close();
+                        File.WriteAllText(@"Login\\datilogin.json", JsonConvert.SerializeObject(User,Formatting.Indented));
+
+
+                        pannelRegistrazione.Visible = false;
+                        PannelloAltoDX.Visible = true;
+                        PannelloAltoSX.Visible = true;
+                        PannelloBassoSX.Visible = true;
+                        PannelloBassoDX.Visible = true;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("La password deve contenere tra gli 8 e i 16 caratteri", "ERRORE:", MessageBoxButtons.OK);
+                        contenitorePasswordInserita.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La password deve contenere almeno una lettera maiuscola e un numero", "ERRORE", MessageBoxButtons.OK);
+                    contenitorePasswordInserita.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("La password non può contenere spazi vuoti", "ERRORE", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
+//creazione classi
+public class Suddito
+{
+    protected string nome;
+    protected string cognome;
+    protected string etàlavorativa; //si o no
+    protected float saldo; //float al posto di double perché occupa 32 bit e non 64
+    protected string mestiere;  //anche se non in età lavorativa, nella casta medievale il figlio sapeva già che avrebbe svolto il mestiere del padre
+    protected int mesiNonPagati;
+    protected float tasseNonPagate;
+    protected int denariSuddTassabile;
+
+    public string Nome
+    {
+        set { nome = value; }
+        get { return nome; }
+    }
+    public string Cognome
+    {
+        set { cognome = value; }
+        get { return cognome; }
+    }
+    public string Etàlavorativa
+    {
+        set { etàlavorativa =value; }
+        get { return etàlavorativa; }
+    }
+    public float Saldo
+    {
+        set { saldo = value; }
+        get { return saldo; }
+    }
+    public string Mestiere
+    {
+        set { mestiere = value; }
+        get { return mestiere; }
+    }
+    public int MesiNonPagati
+    {
+        set { mesiNonPagati = value; }
+        get { return mesiNonPagati; }
+    }
+    public float TasseNonPagate
+    {
+        set { tasseNonPagate = value; }
+        get { return tasseNonPagate; }
+    }
+    public int DenariSuddTassabile
+    {
+        set { denariSuddTassabile = value; }
+        get { return denariSuddTassabile; }
+    }
+}
+public class Tempo
+{
+    protected int mese;
+    protected int anno;
+
+    public int Mese
+    {
+        set { mese = value; }
+        get { return mese; }
+    }
+    public int Anno
+    {
+        set { anno = value; }
+        get { return anno; }
+    }
+}
+public class Stato
+{
+    protected float saldoRE;
+    protected float tesoroStato;
+    protected string condizioneStato;
+
+    public float SaldoRe
+    {
+        set { saldoRE = value; }
+        get { return saldoRE; }
+    }
+    public float TesoroStato
+    {
+        set { tesoroStato = value; }
+        get { return tesoroStato; }
+    }
+    public string CondizioneStato
+    {
+        set { condizioneStato = value; }
+        get { return condizioneStato; }
+    }
+}
+public class Utente
+{
+    protected string password;
+    public string Password
+    {
+        set { password = value; }
+        get { return password; }
     }
 }
